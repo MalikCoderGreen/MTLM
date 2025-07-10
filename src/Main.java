c23cde924c284a6dac52b15598bcb83741e49bee
MaliKCoderGreen <green.malik5@gmail.com>
2025-07-10 13:58:53 -0400

    Week 6: ConcurrentLinkedQueue addition; JSON formatted output of log files

--- src/Main.java
+++ src/Main.java
@@ -15,15 +15,19 @@ Concepts Used: ExecutorService, Callable, Future, ConcurrentHashMap
 
  */
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Logger;
 
 public class Main {
 
+    private static final Logger logger = Logger.getLogger(PrintStatsRunnable.class.getName());
     public static void main(String[] args) throws IOException, InterruptedException {
         Map<String, Map<String, Integer>> logMap = new ConcurrentHashMap<>();
         ReentrantLock alertLock = new ReentrantLock();
@@ -35,74 +39,44 @@ public class Main {
         Path directory = Paths.get(".");
         WatchKey watchKey = directory.register(watchService,
                 StandardWatchEventKinds.ENTRY_CREATE,
-                StandardWatchEventKinds.ENTRY_MODIFY,
-                StandardWatchEventKinds.ENTRY_DELETE);
+                StandardWatchEventKinds.ENTRY_MODIFY);
 
-        // TODO: Add ScheduledExecutor functionality to print real-time stats every 10 seconds
-        // TODO: Fix poll timeout to timeout even if events occurred but no successive events occurred.
-        long lastEventTime = System.currentTimeMillis();
-        while (true) {
-            System.out.println("Beginning of While loop");
-            long currentTime = System.currentTimeMillis();
-            if (lastEventTime > 0 && currentTime - lastEventTime > 5000) {
-                break;
-            }
-            watchKey = watchService.poll(5, TimeUnit.SECONDS);
-
-
-            if (watchKey != null) {
-                System.out.println("HEREE WITH lastEventTime = " + lastEventTime + " and currentTime = " + currentTime);
-                boolean hasRelevant = false;
-                List<WatchEvent<?>> events = watchKey.pollEvents();
-                for (WatchEvent<?> e : events) {
-                    String fileName = e.context().toString();
-                    System.out.println("HERE lvl 0" + " " + fileName + " " + e.kind());
-                    if (e.kind() == StandardWatchEventKinds.ENTRY_CREATE || e.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
-                        System.out.println("HERE lvl 1" + " " + fileName + " " + e.kind());
-                        // Swap files end up being created causing duplicate key counts.
-                        // Using a set because multiple threads keep counting the same file.
-                        if (fileName.startsWith("server") && !fileName.endsWith("~") && inProgressFiles.add(fileName)) {
-                            hasRelevant = true;
-                            System.out.println("HERE lvl 2" + " " + fileName + " " + e.kind());
-                            if (!logMap.containsKey(fileName)) {
-                                logMap.putIfAbsent(fileName, new ConcurrentHashMap<>(Map.of(
-                                        "ERROR", 0, "INFO", 0, "WARN", 0
-                                )));
-                            }
-                            executor.submit(() -> {
-                                try {
-                                    new LogReaderRunnable(logMap, fileName, alertLock).run();
-                                } finally {
-                                    inProgressFiles.remove(fileName);
-                                    System.out.println("Thread: " + Thread.currentThread().getName() + " Removing : " + fileName + " from the set.");
-                                }
-                            });
-                        }
-                    }
-                }
-
-                if (hasRelevant) {
-                    lastEventTime = currentTime;
-                }
-
-                watchKey.reset();
-                System.out.println("Exiting watchKey if statement");
-            }
-
-            System.out.println("Going to next iteration of loop");
-        }
+        // week 6: consumer queue and flush to output.log.
+        ScheduledExecutorService consumerExecutor = Executors.newScheduledThreadPool(1);
+        ConcurrentLinkedQueue<String> logSummaryQueue = new ConcurrentLinkedQueue<>();
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+
             executor.shutdown();
+            consumerExecutor.shutdown();
+
             try {
-                if (!executor.awaitTermination(10, TimeUnit.SECONDS)) {
+                if (!executor.awaitTermination(5, TimeUnit.SECONDS)) {
                     executor.shutdownNow();
+                }
+
+                if (!consumerExecutor.awaitTermination(5, TimeUnit.SECONDS)) {
+                    consumerExecutor.shutdownNow();
                 }
             } catch (InterruptedException e) {
                 System.err.println("Executor termination interrupted");
                 executor.shutdownNow();
+                consumerExecutor.shutdownNow();
             }
 
+            // Flush contents of Queue to output.log
+            if (!logSummaryQueue.isEmpty()) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("output.log", true))) { // append mode
+                    String logSummary;
+                    while ((logSummary = logSummaryQueue.poll()) != null) {
+                        writer.write(logSummary);
+                        writer.newLine();
+                    }
+                    writer.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
             // Close WatchService
             try {
                 watchService.close();
@@ -111,6 +85,69 @@ public class Main {
             }
 
         }));
-    }
 
+        long lastEventTime = System.currentTimeMillis();
+        try {
+            while (true) {
+                logger.info("Beginning of While loop");
+                long currentTime = System.currentTimeMillis();
+                if (lastEventTime > 0 && currentTime - lastEventTime > 10000) {
+                    break;
+                }
+                watchKey = watchService.poll(5, TimeUnit.SECONDS);
+
+                if (watchKey != null) {
+                    //logger.info("HEREE WITH lastEventTime = " + lastEventTime + " and currentTime = " + currentTime);
+                    boolean hasRelevant = false;
+                    List<WatchEvent<?>> events = watchKey.pollEvents();
+                    for (WatchEvent<?> event : events) {
+                        String fileName = event.context().toString();
+                        if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE || event.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
+                            // Swap files end up being created causing duplicate key counts.
+                            // Using a set because multiple threads keep counting the same file.
+                            if (fileName.startsWith("server") && !fileName.endsWith("~") && inProgressFiles.add(fileName)) {
+                                hasRelevant = true;
+                                logger.info("HERE lvl 2" + " " + fileName + " " + event.kind());
+                                if (!logMap.containsKey(fileName)) {
+                                    logMap.putIfAbsent(fileName, new ConcurrentHashMap<>(Map.of(
+                                            "ERROR", 0, "INFO", 0, "WARN", 0
+                                    )));
+                                }
+                                executor.submit(() -> {
+                                    try {
+                                        new LogReaderRunnable(logMap, fileName, alertLock).run();
+                                        // Push to queue
+                                        try {
+                                            LogSummary summary = new LogSummary(fileName, logMap.get(fileName));
+                                            logSummaryQueue.offer(JsonFormatter.prettyPrint(summary));
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                    } finally {
+                                        inProgressFiles.remove(fileName);
+                                        logger.info("Thread: " + Thread.currentThread().getName() + " Removing : " + fileName + " from the set.");
+                                    }
+                                });
+                            }
+                        }
+
+                    }
+
+                    if (hasRelevant) {
+                        lastEventTime = currentTime;
+                    }
+
+                    watchKey.reset();
+                    logger.info("Exiting watchKey if statement");
+
+                }
+            }
+
+        } finally {
+            if (!executor.awaitTermination(10, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+                consumerExecutor.shutdownNow();
+            }
+        }
+    }
 }
